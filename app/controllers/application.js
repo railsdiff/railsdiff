db502ef1f2dcb084da3b21387c1fd742a7eaa19e
Jacob Swanner <jacob@envylabs.com>
2015-01-27 22:18:27 -0500

    Set initial values for selected source and target

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,14 +6,14 @@ export default Ember.Controller.extend({
       this.transitionToRoute('patch', this.get('sourceVersion'), this.get('targetVersion'));
     }
   },
-  sourceVersion: null,
+  sourceVersion: Ember.computed.oneWay('sources.[].1'),
   sources: Ember.computed('model', function() {
     return Ember.keys(this.get('model')).reverse();
   }),
-  targetVersion: null,
+  targetVersion: Ember.computed.oneWay('targets.firstObject'),
   targets: Ember.computed('model', 'sourceVersion', function() {
     var sourceVersion = this.get('sourceVersion');
     if (!sourceVersion) { return []; }
-    return this.get('model')[sourceVersion].reverse();
+    return (this.get('model')[sourceVersion] || []).reverse();
   })
 });
