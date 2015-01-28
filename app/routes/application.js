74f1a91f527dc34be33911970cbddb2776519805
Jacob Swanner <jacob@envylabs.com>
2015-01-27 22:19:31 -0500

    Display error message if versions cannot be fetched

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  versionError: function() {
+    this.controllerFor('application')
+      .set('errorMessage', 'Could not connect to server to load version data.');
+
+    return {};
+  },
+
   model: function() {
-    return this.store.find('version');
+    return this.store.find('version')
+      .then(null, this.versionError.bind(this));
   }
 });
