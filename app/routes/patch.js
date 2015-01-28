305c779eeaec8896b3bdad615d02abdd332b0fdd
Jacob Swanner <jacob@envylabs.com>
2015-01-27 22:25:37 -0500

    Show not found page if patch download 404s

--- app/routes/patch.js
+++ app/routes/patch.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    error: function(e) {
+      if (e && e.jqXHR && e.jqXHR.status === 404) {
+        this.transitionTo('notFound', 'not-found');
+      } else {
+        return true;
+      }
+    }
+  },
+
   model: function(params) {
     this.controllerFor('application')
       .set('sourceVersion', params.source)
