d279bacb89363b920bbc00623bec465b6c268a14
Jacob Swanner <jacob@envylabs.com>
2015-02-06 10:51:24 -0500

    Add infrastructure for about page

--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('patch', {path: '/:source/:target'});
-  this.route("notFound", {path: '/*path'});
+  this.route('about');
+  this.route('notFound', {path: '/*path'});
 });
 
 export default Router;
