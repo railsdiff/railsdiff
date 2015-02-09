fa2a0e958dead5bff71b681241b291815455a65f
Jacob Swanner <jacob@envylabs.com>
2015-02-08 23:41:41 -0500

    Use auto locationType

--- config/environment.js
+++ config/environment.js
@@ -8,7 +8,7 @@ module.exports = function(environment) {
     modulePrefix: 'rails-diff',
     environment: environment,
     baseURL: '/',
-    locationType: 'hash',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
