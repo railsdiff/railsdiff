ed0a3aeaa97af223ab0368a4b216d236c48fb2f8
Jacob Swanner <jacob@envylabs.com>
2015-02-08 23:42:06 -0500

    Validate target version on source change
    Prevents errors that can occur when changing source greater than or
    equal to current selected target.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -7,11 +7,18 @@ export default Ember.Controller.extend(ControllerMessaging, {
       this.transitionToRoute('patch', this.get('sourceVersion'), this.get('targetVersion'));
     }
   },
-  showForm: Ember.computed.and('sources.length', 'targets.length'),
+  showForm: Ember.computed.and('sources.length'),
   sourceVersion: Ember.computed.oneWay('sources.[].1'),
   sources: Ember.computed('model', function() {
     return Ember.keys(this.get('model')).reverse();
   }),
+  targetValidator: function() {
+    var targets = this.get('targets');
+
+    if (!targets.contains(this.get('targetVersion'))) {
+      this.set('targetVersion', targets[targets.length - 1]);
+    }
+  }.observes('targets'),
   targetVersion: Ember.computed.oneWay('targets.firstObject'),
   targets: Ember.computed('model', 'sourceVersion', function() {
     var sourceVersion = this.get('sourceVersion');
