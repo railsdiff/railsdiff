35f9ee05a66840dea424d0e327eab97a7b6f0409
Jacob Swanner <jacob@envylabs.com>
2015-02-25 23:22:34 -0500

    Sort diffs to match old site

--- app/models/patch.js
+++ app/models/patch.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
 import patchSplitter from '../utils/patch-splitter';
 
+var diffSorter = function(a, b) {
+  var aPath = a.get('filePath'),
+      bPath = b.get('filePath');
+
+  if (aPath > bPath) { return 1; }
+  if (aPath < bPath) { return -1; }
+  return 0;
+};
+
 export default Ember.Object.extend({
   diffs: Ember.computed('raw', function() {
     var Diff = this.container.lookupFactory('model:diff');
     return patchSplitter(this.get('raw')).map(function(diff) {
       return Diff.create(diff);
-    });
+    }).sort(diffSorter);
   })
 });
