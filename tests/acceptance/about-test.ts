50fb523c39b5142f59e3aeda16190f86e7270d7c
Nathaniel Bibler <git@nathanielbibler.com>
2020-10-11 14:40:46 -0400

    Add tests for about page

---
+++ tests/acceptance/about-test.ts
@@ -0,0 +1,22 @@
+import { currentURL, visit } from "@ember/test-helpers";
+import { click } from "ember-semantic-test-helpers/test-support";
+import { module, test } from "qunit";
+
+import sample from "../../mirage/scenarios/sample";
+import { setupAcceptanceTest } from "../helpers";
+
+module("Acceptance | about", (hooks) => {
+  setupAcceptanceTest(hooks);
+
+  hooks.beforeEach(() => {
+    sample(server);
+  });
+
+  test("describes the application", async (assert) => {
+    await visit("/");
+    await click("About");
+
+    assert.equal(currentURL(), "/about");
+    assert.dom(".content").containsText("Why RailsDiff was built");
+  });
+});
