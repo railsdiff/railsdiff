6951a009328fe3df740ef8b6c6d07cd3313e047e
Nathaniel Bibler <git@nathanielbibler.com>
2021-04-16 16:16:53 -0400

    Enable Embroider's static addon trees

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,5 +21,8 @@ module.exports = function (defaults) {
   // along with the exports of each module as its value.
 
   const { Webpack } = require("@embroider/webpack");
-  return require("@embroider/compat").compatBuild(app, Webpack);
+  return require("@embroider/compat").compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+  });
 };
