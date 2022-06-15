ccd3c4608f8d311ec514c71ddd714ea2019541af
Nathaniel Bibler <git@nathanielbibler.com>
2022-06-14 21:51:21 -0400

    build(deps-dev): replace faker with @faker-js/faker

--- mirage/factories/owner.ts
+++ mirage/factories/owner.ts
@@ -1,4 +1,4 @@
-import faker from "faker";
+import { faker } from "@faker-js/faker";
 import { Factory } from "miragejs";
 
 export default Factory.extend({
--- mirage/factories/tag.ts
+++ mirage/factories/tag.ts
@@ -1,4 +1,4 @@
-import faker from "faker";
+import { faker } from "@faker-js/faker";
 import { Factory } from "miragejs";
 import { FileMap } from "rails-diff/helpers/compare";
 
