da041f27e3fdc3358ea1e33ea6907e6e8e57c009
Nathaniel Bibler <git@nathanielbibler.com>
2022-07-02 11:29:33 -0400

    refactor: remove duplicate IndexController implementation
    The previous implementation was apparently a direct copy of the
    ApplicationController.

--- app/controllers/index.ts
+++ app/controllers/index.ts
@@ -1,32 +1,3 @@
-import Controller from "@ember/controller";
-import { action } from "@ember/object";
-import { Registry as ServiceRegistry, inject as service } from "@ember/service";
-import ApplicationRoute from "rails-diff/routes/application";
+import ApplicationController from "./application";
 
-export default class IndexController extends Controller {
-  declare model: Awaited<ReturnType<ApplicationRoute["model"]>>;
-
-  @service("router")
-  private _router!: ServiceRegistry["router"];
-
-  @action
-  setSource(source: string) {
-    this.versions.setSource(source);
-  }
-
-  @action
-  setTarget(target: string) {
-    this.versions.setTarget(target);
-  }
-
-  @action
-  showDiff(event: Event) {
-    event.preventDefault();
-    this._router.transitionTo(
-      `/${this.versions.source}/${this.versions.target}`
-    );
-  }
-
-  @service("versions")
-  versions!: ServiceRegistry["versions"];
-}
+export default class IndexController extends ApplicationController {}
